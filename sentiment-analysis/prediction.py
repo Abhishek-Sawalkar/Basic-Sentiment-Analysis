c58aa8825205e0565b7d73e1ced0812ad2a3ee79
Abhishek-Sawalkar <abhikuroko@gmail.com>
2020-05-22 03:02:01 +0530

    Add files via upload

---
+++ sentiment-analysis/prediction.py
@@ -0,0 +1,38 @@
+import sys
+import gzip
+import pickle
+
+CLASSES = {
+    0: "negative",
+    4: "positive"
+}
+
+
+def load_model(model_filename):
+    """ Load model from file """
+    print("loading the model...")
+    try:
+        with gzip.open(model_filename, 'rb') as fmodel:
+            model = pickle.load(fmodel)
+    except Exception as ex:
+        raise IOError("Couldn't load  model: %r" % ex)
+
+    return model
+
+def predict(model, text):
+    """ Predict class given model and input (text) """
+    print("Extracting features...")
+    x_vector = model.vectorizer.transform([text])
+    y_predicted = model.predict(x_vector)
+    return CLASSES.get(y_predicted[0])
+
+def main(argv):
+    """ Predict the sentiment of the given text """
+    text = argv[1]
+    model_filename = "/kaggle/working/model.dat.gz"
+    model = load_model(model_filename)
+    print(predict(model, text))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
